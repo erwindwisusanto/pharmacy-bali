ee888eed9c75b767c8daf1cc7daa2baa4a8313f9
erwin dwi s <erwin@waki.mobi>
2024-06-21 09:18:50 +0700

    add homepage

---
+++ resources/views/index.blade.php
@@ -0,0 +1,126 @@
+<x-master-layout>
+    <div class="content">
+        <section class="banner-pages homepage">
+            <div class="container">
+                <div class="text">
+                    <p class="subtitle">Pharmacy Bali</p>
+                    <h3 class="fw-bold">
+                        Your Trusted Online Pharmacy!
+                    </h3>
+                    <p>
+                        Get Your Medications Delivered to Your Doorstep
+                    </p>
+                    <a href="https://api.whatsapp.com/send/?phone=6285212500030" class="btn btn-warning fs-14">Order Now
+                    </a>
+                </div>
+            </div>
+
+        </section>
+
+        <section class="why">
+            <div class="container">
+                <div class="text-center">
+                    <div class="title-icon">
+                        <div class="text">
+                            <span>Why Choose</span>
+                            <h5>Pharmacy Bali ?</h5>
+                        </div>
+                        <img src="assets/img/img-hospital.png" alt="">
+                    </div>
+                </div>
+                <div class="row justify-content-center mt-4">
+                    <div class="col-md-10">
+                        <img src="assets/img/why-pharmacy.svg" class="d-none d-md-block w-100" alt="">
+                        <img src="assets/img/why-pharmacy-mobile.svg" class="d-block d-md-none w-100" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="how">
+            <div class="container">
+                <img src="assets/img/how-work.svg" class="d-none d-md-block w-100" alt="">
+                <img src="assets/img/how-work-mobile.svg" class="d-block d-md-none w-100" alt="">
+            </div>
+        </section>
+
+        <section class="testimonial">
+            <div class="container">
+                <div class="text-center">
+                    <div class="title-icon">
+                        <div class="text">
+                            <span>Customer</span>
+                            <h5>Testimonials</h5>
+                        </div>
+                        <img src="assets/img/img-testimonial.png" alt="">
+                    </div>
+                </div>
+                <div class="swiper swiper-article mt-5">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="card-slide-testimoni">
+                                <div class="top">
+                                    <p class="say">
+                                        "The pharmacists were very helpful in answering my questions. Highly recommend!"
+                                    </p>
+                                </div>
+                                <div class="name">
+                                    <img src="assets/img/avatar/img-user01.png" alt="">
+                                    <div class="rate d-flex g-2">
+                                        <i class="mdi mdi-star text-warning"></i>
+                                        <i class="mdi mdi-star text-warning"></i>
+                                        <i class="mdi mdi-star text-warning"></i>
+                                        <i class="mdi mdi-star text-warning"></i>
+                                        <i class="mdi mdi-star text-warning"></i>
+                                    </div>
+                                    <h6>Sarah K.</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="card-slide-testimoni">
+                                <div class="top">
+                                    <p class="say">
+                                        "Excellent service! My medications arrived on time and the process was so easy."
+                                    </p>
+                                </div>
+                                <div class="name">
+                                    <img src="assets/img/avatar/img-user02.png" alt="">
+                                    <div class="rate d-flex g-2">
+                                        <i class="mdi mdi-star text-warning"></i>
+                                        <i class="mdi mdi-star text-warning"></i>
+                                        <i class="mdi mdi-star text-warning"></i>
+                                        <i class="mdi mdi-star text-warning"></i>
+                                        <i class="mdi mdi-star text-warning"></i>
+                                    </div>
+                                    <h6>Jane D.</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="card-slide-testimoni">
+                                <div class="top">
+                                    <p class="say">
+                                        "Great selection of products and very user-friendly website."
+                                    </p>
+                                </div>
+                                <div class="name">
+                                    <img src="assets/img/avatar/img-user03.png" alt="">
+                                    <div class="rate d-flex g-2">
+                                        <i class="mdi mdi-star text-warning"></i>
+                                        <i class="mdi mdi-star text-warning"></i>
+                                        <i class="mdi mdi-star text-warning"></i>
+                                        <i class="mdi mdi-star text-warning"></i>
+                                        <i class="mdi mdi-star text-warning"></i>
+                                    </div>
+                                    <h6>Mark T.</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+</x-master-layout>
