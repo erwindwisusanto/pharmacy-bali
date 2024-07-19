245425c7e24ca20d4c2e58871224bb62915a518b
erwin dwi s <erwin@waki.mobi>
2024-07-19 20:57:14 +0700

    feat: wa direct to contact

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -52,7 +52,7 @@
         const baseUrl = "{{ request()->root() }}";
         const campaignName = "{{ request()->query('camp') }}";
         const _SOURCE = "pharmacy_bali";
-        const numberphone = "6285212500030";
+        const numberphone = "6282221122311";
         const telegramUsername = 'cepat_sehat';
         const _WHATSAPP = "whatsapp";
         const _TELEGRAM = "telegram";
@@ -137,11 +137,11 @@
                 case _WHATSAPP:
                     if (campaignName) {
                         updateCounter(_WHATSAPP);
-                        window.open(`https://web.WhatsApp.com/send?phone=${encodeURIComponent(numberphone)}&text=${encodeURIComponent(waword)}`, '_blank');
+                        window.open(`https://api.whatsapp.com/send/?phone=${encodeURIComponent(numberphone)}&text=${encodeURIComponent(waword)}`, '_blank');
                         break;
                     } else {
                         updateCounter(_WHATSAPP);
-                        window.open(`https://api.whatsapp.com/send/?phone=6282221122311&text=Hello+denguehospital.com+by+Cepat+Sehat+Clinic%2C+I+want+a+consultation&type=phone_number&app_absent=0`, '_blank');
+                        window.open(`https://api.whatsapp.com/send/?phone=${encodeURIComponent(numberphone)}&text=Hello+denguehospital.com+by+Cepat+Sehat+Clinic%2C+I+want+a+consultation&type=phone_number&app_absent=0`, '_blank');
                         break;
                     }
                 case _TELEGRAM:
